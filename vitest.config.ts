6a416b0ad3f0082f3b7da361710c804a93857512
ayapapa <ayapapajapan@yahoo.co.jp>
2026-09-06 17:28:06 +0900

    Modified vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,29 @@
-/**
- * The execution order of test files is serialized for the following reason:
- *  - As part of the `FileLock.setConfig()` test, the specified lock directory—or 
- *    `.lock` in the runtime directory if no lock directory is specified—is forcibly deleted; 
- *    this can leave the file locking mechanism itself in a compromised state
- *   (i.e., the file expected to store lock information is missing).
- */
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts', // Match tsup's src/index.ts
+      formats: ['es', 'cjs'], // Match --format esm,cjs
+      fileName: (format) => format === 'cjs' ? 'index.cjs' : 'index.js',
+    },
+    outDir: 'dist', // Match　--out-dir dist 
+    // For type definitions (--dts), `vite-plugin-dts` is required,
+    // so you need to add the plugin configuration.
+  },
+  // Write settings to disable only in the test environment here.
   test: {
-    fileParallelism: false,
+    // File patterns to exclude
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    
+    // Coverage measurement settings
+    coverage: {
+      reporter: ['text', 'text-summary', 'json-summary', 'json', 'html'],
+      include: ['src/**/*.{ts,tsx,js,jsx}'],
+    },
+    
+    // Root directory
+    root: '.',
   },
 })
